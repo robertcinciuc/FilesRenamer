b3e49f781a09fd9075e3801bea9e9f1b673813bf
Robert Cinciuc <robertcinciuc@gmail.com>
2023-10-17 22:31:05 +0200

    Added support for zero padding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,11 +11,13 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        int start = 0;
-        if(args.length != 1){
-            throw new Exception("Need to input the starting index to rename the files");
+        int index = 0;
+        int len = 0;
+        if(args.length != 2){
+            throw new Exception("Need to input the starting index & length of numbers to rename the files");
         }
-        start = Integer.parseInt(args[0]);
+        index = Integer.parseInt(args[0]);
+        len = Integer.parseInt(args[1]);
 
         File resources = new File(Paths.get(".", "resources").toString());
 
@@ -34,9 +36,11 @@ public class Main {
         Collections.sort(filePaths);
 
         for(Path filePath : filePaths){
+            String actualFormat = "%0" + len + "d";
             String fileExtension = Files.getFileExtension(filePath.getFileName().toString());
-            File newFile = new File(start +  "." + fileExtension);
-            Path newFilePath = Paths.get(".", "resources", start +  "." + fileExtension);
+            String newFullName = String.format(actualFormat, index) +  "." + fileExtension;
+            File newFile = new File(newFullName);
+            Path newFilePath = Paths.get(".", "resources", newFullName);
 
             if(newFile.exists()){
                 throw new Exception("A file with the new name already exists");
@@ -44,7 +48,7 @@ public class Main {
 
             java.nio.file.Files.move(filePath, newFilePath);
 
-            start++;
+            index++;
         }
     }
 }
